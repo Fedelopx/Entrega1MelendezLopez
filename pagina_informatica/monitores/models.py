72971736866e2039ff10d4a760f447fd7af62695
Federico Lopez <fede.lopez.tejera@gmail.com>
2022-08-06 16:08:23 -0300

    appmodels

--- pagina_informatica/monitores/models.py
+++ pagina_informatica/monitores/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Monitores (models.Model): 
+    nombre = models.CharField(max_length=200)
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=100)
+    pulgadas = models.CharField(max_length=50)
+    precio = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    stock = models.IntegerField()
+
+    def __str__(self): 
+        return self.nombre
