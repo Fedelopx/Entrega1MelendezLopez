c256fc5c967313a15ef8be65410e4bc3ed963acc
FedericoMelendez <federiconahuelmelendez@gmail.com>
2022-08-07 16:54:59 -0300

    crear notebook

--- pagina_informatica/notebooks/views.py
+++ pagina_informatica/notebooks/views.py
@@ -1,32 +1,25 @@
+from itertools import product
 from multiprocessing import context
-from django.shortcuts import redirect,render
+from django.shortcuts import render
 from notebooks.models import Notebooks
-from notebooks.forms import Formulario_notebooks
 
 def crear_notebook(request):
-    
-    if request.method == 'POST':
-        form = Formulario_notebooks(request.POST)
+    nueva_notebook= Notebooks.objects.create(
+        nombre= 'Notebook', 
+        marca='Dell', 
+        modelo='Inspiron', 
+        procesador='Intel Core i7',
+        ram=' 8GB',
+        pulgadas='15,6',
+        almacenamiento='1 TB SSD',
+        precio= 100405,
+        stock=20
+        )
+    context={
+        'nueva_notebook':nueva_notebook
+    }
+    return render(request, 'notebooks/nueva_notebook.html', context=context)
 
-        if form.is_valid():
-            Notebooks.objects.create(
-                nombre = form.cleaned_data['nombre'],
-                marca = form.cleaned_data['marca'],
-                modelo = form.cleaned_data['modelo'],
-                procesador = form.cleaned_data['procesador'],
-                ram = form.cleaned_data['ram'],
-                pulgadas= form.cleaned_data['pulgadas'],
-                almacenamiento = form.cleaned_data['almacenamiento'],
-                precio = form.cleaned_data['precio'],
-                stock= form.cleaned_data['stock'],
-            )
-            
-            return redirect(lista_notebooks)
-
-    elif request.method == 'GET':
-        form = Formulario_notebooks()
-        context = {'form':form}
-        return render(request, 'notebooks/nueva_notebook.html', context=context)
 
 def lista_notebooks(request):
     notebooks = Notebooks.objects.all() 
--- pagina_informatica/pagina_informatica/urls.py
+++ pagina_informatica/pagina_informatica/urls.py
@@ -1,8 +1,11 @@
-
+import imp
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from pagina_informatica.views import saludo
+from notebooks.views import crear_notebook,lista_notebooks
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path ('saludo/', saludo, name='saludo'),
+    path('notebooks/',include('notebooks.urls'))
 ]
