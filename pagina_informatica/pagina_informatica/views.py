e03aa115ca0aa9fec1112c9101e69ce53203c6f0
FedericoMelendez <federiconahuelmelendez@gmail.com>
2022-08-07 17:09:54 -0300

    crear notebooks

--- pagina_informatica/pagina_informatica/views.py
+++ pagina_informatica/pagina_informatica/views.py
@@ -2,7 +2,6 @@ from importlib.resources import contents
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
-from datetime import  datetime
 
 def saludo (request):
     return HttpResponse('Bienvenidos a todos ')
